985374836ce06bdcbf59b6cddc7267da54ec8c92
Fahad Qazi <fahadqazi@gmail.com>
2017-03-06 22:26:33 +0000

    respond to correct and wrong clicks

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.getElementById('message');
+var h1 = document.querySelector('h1');
 
 colorDisplay.textContent = pickedColor
 
@@ -19,10 +13,11 @@ for (var i=0; i<squares.length; i++){
 
     squares[i].addEventListener('click', function(){
         var clickedColor = this.style.background;
-
+        // console.log(pickedColor, clickedColor);
         if (clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!"
             changeColors(clickedColor)
+            h1.style.background = clickedColor;
         } else {
             this.style.background = "#232323"
             message.textContent = "Try Aain"
@@ -40,4 +35,21 @@ function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     console.log(random);
     return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr = [];
+    for (var i=0; i<num; i++){
+        arr.push(randomColor());
+
+    }
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b +")";
 }
